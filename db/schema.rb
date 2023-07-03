0629a6444d005a934a3f781e827fbaaa05891d64
naveed <naveed.fiaz@whizzbridge.com>
2023-07-03 10:11:18 +0500

    added industries list in english and arabic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_123518) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_120607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -116,6 +116,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_123518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name_ar"
+  end
+
+  create_table "industries", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "name_ar", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "investor_profiles", force: :cascade do |t|
