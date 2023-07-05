fc9a5c65ae9629cf64ec153d67443405daa91326
naveed <naveed.fiaz@whizzbridge.com>
2023-07-05 11:39:49 +0500

    minor tweaks

--- app/models/syndicate_profile.rb
+++ app/models/syndicate_profile.rb
@@ -3,12 +3,4 @@
 class SyndicateProfile < ApplicationRecord
   belongs_to :syndicate
   has_one :attachment, as: :parent, dependent: :destroy
-
-  after_update :update_user_status
-
-  private
-
-  def update_user_status
-    syndicate.update(status: User.statuses[:submitted])
-  end
 end
