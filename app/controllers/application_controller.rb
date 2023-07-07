fc0505c1612247e5c81e08060961cc262ab64af2
naveed <naveed.fiaz@whizzbridge.com>
2023-07-07 17:11:06 +0500

    update response incase of unconfirmed access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,15 @@ class ApplicationController < ActionController::API
   include ExceptionHandler
 
   before_action :authenticate_user!
+  before_action :block_unconfirmed_access
   before_action :set_locale
   respond_to :json
+
+  def block_unconfirmed_access
+    return if current_user.confirmed?
+
+    failure(I18n.t('devise.failure.unconfirmed'), current_user)
+  end
 
   def set_locale
     return unless current_user
