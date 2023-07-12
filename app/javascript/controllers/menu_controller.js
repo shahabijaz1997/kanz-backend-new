ec6b26ba72fa80dd9f8903ece86c15b7d28ee2d7
Muhammad Ali Shah <m.alisherazi99@gmail.com>
2023-07-12 15:39:16 +0500

    Hide user menu on click outside.

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,12 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from 'stimulus-use'
 import Dom from '../dom/manipulator'
 
 export default class extends Controller {
   static targets = ['button', 'dropdown']
 
+  connect() {
+    useClickOutside(this)
+  }
+
   toggleMenu(event) {
-    console.log('Toggle')
     event.preventDefault()
     Dom.toggleClass(this.dropdownTarget, 'show')
+  }
+
+  clickOutside(event) {
+    event.preventDefault()
+    Dom.removeClass(this.dropdownTarget, 'show')
   }
 }
