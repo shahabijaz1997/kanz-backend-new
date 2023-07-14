a0fbe97abf7ef9c4ee9c068183b9192c3955f330
Muhammad Ali Shah <m.alisherazi99@gmail.com>
2023-07-14 11:40:05 +0500

    Added audit gem migration.

--- db/seeds/admin_roles.rb
+++ db/seeds/admin_roles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-admin_roles = ['Admin', 'Super Admin', 'Customer Support Rep', 'Compliance Officer']
+admin_roles = ['Super Admin', 'Admin', 'Customer Support Rep', 'Compliance Officer']
 
 admin_roles.each do |admin_role|
   AdminRole.find_or_create_by(title: admin_role)
