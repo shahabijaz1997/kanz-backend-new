6ee33c6b79a5b459e0b6f3aac07e36f1c101003a
naveed <naveed.fiaz@whizzbridge.com>
2023-10-05 20:25:40 +0500

    minor fixes

--- app/serializers/feature_serializer.rb
+++ app/serializers/feature_serializer.rb
@@ -4,19 +4,5 @@
 class FeatureSerializer
   include JSONAPI::Serializer
 
-  attributes :id
-
-  attribute :en do |feature|
-    { 
-      title: feature.title,
-      description: feature.description
-    }
-  end
-
-  attribute :ar do |feature|
-    { 
-      title: feature.title_ar,
-      description: feature.description_ar
-    }
-  end
+  attributes :id, :title, :description
 end
--- app/serializers/property_detail_serializer.rb
+++ app/serializers/property_detail_serializer.rb
@@ -3,8 +3,8 @@
 # Details json serializer
 class PropertyDetailSerializer
   include JSONAPI::Serializer
-  attributes :state, :city, :area, :location, :building_name, :street_address, :size
-             :has_bedrooms, :no_bedrooms, :has_kitchen, :no_kitchen, :has_washroom
+  attributes :state, :city, :area, :location, :building_name, :street_address, :size,
+             :has_bedrooms, :no_bedrooms, :has_kitchen, :no_kitchen, :has_washroom,
              :no_washrooms, :has_parking, :parking_capacity, :has_swimming_pool,
              :is_rental, :rental_period, :rental_amount, :dividend_yeild, :yearly_appreciation,
              :external_links
@@ -23,7 +23,9 @@ class PropertyDetailSerializer
 
   private
 
-  def option_value(id)
-    Option.find_by(id: id)&.statement
+  class << self
+    def option_value(id)
+      Option.find_by(id: id)&.statement
+    end
   end
 end
