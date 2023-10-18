9a3e41e6bff0811005d1cc41c4bdd2e980492a81
naveed <naveed.fiaz@whizzbridge.com>
2023-10-18 12:37:14 +0500

    Added association for options

--- app/models/property_detail.rb
+++ app/models/property_detail.rb
@@ -6,4 +6,20 @@ class PropertyDetail < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     %w[size rental_amount is_rental]
   end
+
+  belongs_to :swimming_pool_type, class_name: 'Option', foreign_key: 'swimming_pool_type'
+  belongs_to :rental_period, class_name: 'Option', foreign_key: 'rental_period'
+  belongs_to :country, class_name: 'Option', foreign_key: 'country_id'
+
+  def swimming_pool
+    swimming_pool_type&.statement
+  end
+
+  def rental_duration
+    rental_period&.statement
+  end
+
+  def country_name
+    country&.statement
+  end
 end
