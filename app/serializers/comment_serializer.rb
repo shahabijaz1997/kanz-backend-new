8be1ac4d8a7240f8fff1ee4408840312affc0cdb
naveed <naveed.fiaz@whizzbridge.com>
2023-10-27 12:44:55 +0500

    minor tweaks

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -9,4 +9,8 @@ class CommentSerializer
   attribute :author_name do |comment|
     comment.author.name
   end
+
+  attribute :created_at do |comment|
+    DateTime.parse(comment.created_at.to_s).strftime('%d/%m/%Y %I:%M:%S %p')
+  end
 end
