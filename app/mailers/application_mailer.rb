04ba32c5c1bdf53b8804cf1df34d988822716532
naveed <naveed.fiaz@whizzbridge.com>
2023-11-01 11:44:46 +0500

    minor tweaks

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV.fetch('FROM_EMAIL', nil)
   layout 'mailer'
+
+  rescue_from StandardError do |excep|
+    Rails.logger.error ([excep.message]+excep.backtrace).join($/)
+  end
 end
