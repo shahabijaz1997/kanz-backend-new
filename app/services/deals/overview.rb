f3e82710882f7786f9e79a97257107f2cb955952
naveed <naveed.fiaz@whizzbridge.com>
2023-11-01 13:01:34 +0500

    added usps and external links to syndicate overview

--- app/services/deals/overview.rb
+++ app/services/deals/overview.rb
@@ -53,29 +53,54 @@ module Deals
       property_detail = deal.property_detail
       return {} if property_detail.blank?
 
-      params = {
+      {
         expected_dividend_yield: property_detail.dividend_yeild,
         expected_annual_return: property_detail.yearly_appreciation,
         size: property_detail.size,
-        features: {},
-        location: property_detail.location_detail
+        features: property_features(property_detail),
+        location: property_detail.location_detail,
+        unique_selling_points: property_usps,
+        external_links: external_links
+        terms: terms
       }
+    end
 
-      params[:features][:has_bedrooms] = property_detail.has_bedrooms
-      params[:features][:bedrooms] = property_detail.no_bedrooms if property_detail.has_bedrooms
-      params[:features][:has_kitchen] = property_detail.has_kitchen
-      params[:features][:kitchen] = property_detail.no_kitchen if property_detail.has_kitchen
-      params[:features][:has_washroom] = property_detail.has_washroom
-      params[:features][:washrooms] = property_detail.no_washrooms if property_detail.has_washroom
-      params[:features][:has_parking] = property_detail.has_parking
-      params[:features][:parking_space] = property_detail.parking_capacity if property_detail.has_parking
-      params[:features][:has_swimming_pool] = property_detail.has_swimming_pool
-      params[:features][:swimming_pool] = property_detail.swimming_pool_type if property_detail.has_swimming_pool
-      params[:features][:is_rental] = property_detail.is_rental
-      params[:features][:rental_amount] = property_detail.rental_amount if property_detail.is_rental
-      params[:features][:rental_period] = property_detail.rental_duration if property_detail.is_rental
+    def property_features(features)
+      feature = {}
+      feature[:has_bedrooms] = features.has_bedrooms
+      feature[:bedrooms] = features.no_bedrooms if features.has_bedrooms
+      feature[:has_kitchen] = features.has_kitchen
+      feature[:kitchen] = features.no_kitchen if features.has_kitchen
+      feature[:has_washroom] = features.has_washroom
+      feature[:washrooms] = features.no_washrooms if features.has_washroom
+      feature[:has_parking] = features.has_parking
+      feature[:parking_space] = features.parking_capacity if features.has_parking
+      feature[:has_swimming_pool] = features.has_swimming_pool
+      feature[:swimming_pool] = features.swimming_pool_type if features.has_swimming_pool
+      feature[:is_rental] = features.is_rental
+      feature[:rental_amount] = features.rental_amount if features.is_rental
+      feature[:rental_period] = features.rental_duration if features.is_rental
 
-      params
+      feature
+    end
+
+    def property_usps
+      deal.features.map do |usp|
+        {
+          title: usp.title,
+          description: usp.description
+        }
+      end
+    end
+
+    def external_links
+      deal.external_links.map do |link|
+        link.url
+      end
+    end
+
+    def terms
+      FieldAttribute.find_by(field_mapping: 'agreed_with_kanz_terms')&.description
     end
 
     def docs
