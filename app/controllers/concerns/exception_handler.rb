f53f661b2c0ec619764feaa2c3408529f8b0d82d
naveed <naveed.fiaz@whizzbridge.com>
2023-11-23 14:19:13 +0500

    minor tweaks

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -5,26 +5,32 @@ module ExceptionHandler
 
   included do
     rescue_from ActionController::InvalidAuthenticityToken do |_e|
+      Rails.logger.error ([e.message]+e.backtrace).join($/)
       render json: { message: I18n.t('errors.exceptions.auth_token_expired') }, status: :unauthorized
     end
 
     rescue_from ActiveRecord::RecordNotFound do |_e|
+      Rails.logger.error ([e.message]+e.backtrace).join($/)
       render json: { message: I18n.t('errors.exceptions.not_found') }, status: :not_found
     end
 
     rescue_from ActiveRecord::RecordNotUnique do |_e|
+      Rails.logger.error ([e.message]+e.backtrace).join($/)
       render json: { message: I18n.t('errors.exceptions.existing_record') }, status: :unprocessable_entity
     end
 
     rescue_from ActionController::ParameterMissing do |_e|
+      Rails.logger.error ([e.message]+e.backtrace).join($/)
       render json: { message: I18n.t('errors.exceptions.parameter_missing') }, status: :unprocessable_entity
     end
 
     rescue_from Pundit::NotAuthorizedError do |_e|
+      Rails.logger.error ([e.message]+e.backtrace).join($/)
       render json: { message: I18n.t('errors.exceptions.unauthorized') }, status: :forbidden
     end
 
     rescue_from ArgumentError do |e|
+      Rails.logger.error ([e.message]+e.backtrace).join($/)
       render json: { message: e.message }, status: :unprocessable_entity
     end
 
