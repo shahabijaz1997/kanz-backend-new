71b3e59e81be9a83b61f14761b5476b39af2028a
naveed <naveed.fiaz@whizzbridge.com>
2023-11-30 16:22:16 +0500

    minor tweaks

--- app/services/deals/overview.rb
+++ app/services/deals/overview.rb
@@ -35,7 +35,9 @@ module Deals
         status: deal.status,
         start_at: deal.start_at.blank? ? '' : Date.parse(deal.start_at.to_s).strftime('%d/%m/%Y'),
         end_at: deal.end_at.blank? ? '' : Date.parse(deal.end_at.to_s).strftime('%d/%m/%Y'),
-        token: deal.token
+        token: deal.token,
+        is_invested: user.investments.present?,
+        my_invested_amount: user.investments.find_by(deal_id: deal.id).amount
       }
 
       additional_params = deal.startup? ? startup_params : property_params
