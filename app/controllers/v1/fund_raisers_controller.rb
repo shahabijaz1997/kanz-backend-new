4cee5de3c9391a4eff0041b3e63b931fb2518d84
naveed <naveed.fiaz@whizzbridge.com>
2023-12-06 19:05:41 +0500

    minor tweaks

--- app/controllers/v1/fund_raisers_controller.rb
+++ app/controllers/v1/fund_raisers_controller.rb
@@ -8,11 +8,11 @@ module V1
 
     def show
       fund_raiser_attributes = FundRaiserSerializer.new(@fund_raiser).serializable_hash[:data][:attributes]
-      success(I18n.t('fund_raiser.get.success.show'), startup_attributes)
+      success(I18n.t('fund_raiser.get.success.show'), fund_raiser_attributes)
     end
 
     def create
-      profile = @fund_raiser.profile || FundRaiserProfile.new(startup_id: @fund_raiser.id)
+      profile = @fund_raiser.profile || FundRaiserProfile.new(fund_raiser_id: @fund_raiser.id)
       FundRaiserProfile.transaction do
         profile.update!(profile_params.except(:logo))
         Attachment.upload_file(profile, profile_params[:logo]) if profile_params[:logo].present?
@@ -25,28 +25,28 @@ module V1
     private
 
     def profile_params
-      return [] unless params[:startup_profile][:step].to_i.in?([1,2])
+      return [] unless params[:fund_raiser_profile][:step].to_i.in?([1,2])
 
-      if params[:startup_profile][:step].to_i == 1
-        params.require(:startup_profile).permit(
+      if params[:fund_raiser_profile][:step].to_i == 1
+        params.require(:fund_raiser_profile).permit(
           :step, :company_name, :legal_name, :country_id, :website, :address, industry_ids: []
         )
       else
-        params.require(:startup_profile).permit(
+        params.require(:fund_raiser_profile).permit(
           :step, :logo, :description, :ceo_name, :ceo_email, :total_capital_raised,
           :current_round_capital_target, :currency
         )
       end
     end
 
-    def validate_startup
+    def validate_fund_raiser
       return unprocessable unless current_user.fund_raiser?
 
       @fund_raiser = current_user
     end
 
     def check_file_presence
-      return if @fund_raiser.profile&.attachment || params[:startup_profile][:step].to_i == 1
+      return if @fund_raiser.profile&.attachment || params[:fund_raiser_profile][:step].to_i == 1
 
       failure(I18n.t('errors.exceptions.file_missing')) if profile_params[:logo].blank?
     end
