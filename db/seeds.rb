6fcb29733ab3b8bef566c5ffc0f585b4a97d3fde
naveed <naveed.fiaz@whizzbridge.com>
2023-12-26 20:30:43 +0500

    minor tweak

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+# 
 
-file_names = [
-  'user_roles', 'admin_roles', 'countries', 'industries', 'regions',
-  'user_attachments_config', 'deal_stepper', 'dependency_tree']
+file_names = ['user_roles', 'admin_roles', 'countries', 'industries', 'regions', 'user_attachments_config', 'questions' , 'deal_stepper', 'dependency_tree']
+
+file_names = ['questions']
 
 file_names.each do |file_name|
   load Rails.root.join('db', 'seeds', "#{file_name}.rb")
