b2e0bc9dbd7b9481b011415ef27267f4c1060a32
naveed <naveed.fiaz@whizzbridge.com>
2024-02-14 19:31:12 +0500

    minor tweaks

--- db/migrate/20240214072743_add_column_to_store_multiple.rb
+++ db/migrate/20240214072743_add_column_to_store_multiple.rb
@@ -1,6 +1,11 @@
 class AddColumnToStoreMultiple < ActiveRecord::Migration[7.0]
-  def change
-    add_column :deals, :valuation_multiple, :float, default: 1.0
-    add_column :deals, :markets, :string, array: true, default: []
+  def up
+    # add_column :deals, :valuation_multiple, :float, default: 1.0
+    # add_column :deals, :markets, :string, array: true, default: []
+  end
+
+  def down
+    change_column :deals, :valuation_multiple, :float, default: 1.0
+    change_column :deals, :markets, :string, array: true, default: []
   end
 end
