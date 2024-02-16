62fc0ab1358ade80c269aae43c1cf75a7822b15e
naveed <naveed.fiaz@whizzbridge.com>
2024-02-16 16:09:30 +0500

    Added deal update publishing

--- app/models/deal_update.rb
+++ app/models/deal_update.rb
@@ -9,8 +9,10 @@ class DealUpdate < ApplicationRecord
   validates :description, presence: true
   validate :deal_updatable
 
+  before_validation :set_directory_path
+
   def report_url
-    Rails.env.development? ? ActiveStorage::Blob.service.path_for(report.key) : report.url if report.attached?
+    report.url if report.attached?
   end
 
   private
@@ -19,5 +21,10 @@ class DealUpdate < ApplicationRecord
     return errors.add(:deal, I18n.t('deal_update.deal_not_closed')) unless deal.closed?
     return errors.add(:user, I18n.t('deal_update.not_allowed')) if deal.syndicate? && added_by_id != deal.syndicate_id
     return errors.add(:user, I18n.t('deal_update.not_allowed')) if deal.classic? && added_by_id != deal.user_id
+  end
+
+  def set_directory_path
+    gust = SecureRandom.base36(28)
+    report.key = "deal_update/#{deal_id}/#{gust}" if report.new_record?
   end
 end
